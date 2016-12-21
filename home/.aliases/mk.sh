7888208521ec5219fee54a665b158a0f7cde14cb
Niv Ben-David <nivbend@gmail.com>
2016-12-21 18:25:55 +0200

    Fixup of mkproj

--- home/.aliases/mk.sh
+++ home/.aliases/mk.sh
@@ -4,8 +4,13 @@ mkproj()
 
     git init "$HOME/projects/$dir"
     cd "$HOME/projects/$dir"
-    touch .gitignore
+
+    echo '*.~' >> .gitignore
+    echo '*.pyc' >> .gitignore
     git add .gitignore
+
+    git commit -m "Create repository"
+    echo "Update .gitignore if neccessary and run 'git commit -a --ammend --no-edit'"
 }
 
 mkmod()
